25e8b7297edb33906e5cc0c04d090acb27f7542f
WangJunZzz <510423039@qq.com>
2022-08-04 14:07:11 +0800

    👕 调整项目结构

---
+++ src/Bee.Abp/Bee.Abp.Apm/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Bee.Abp.Apm
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Bee/Bee.Apm/Microsoft/AspNetCore/ApplicationBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Elastic.Apm.DiagnosticSource;
+using Elastic.Apm.Elasticsearch;
+using Elastic.Apm.EntityFrameworkCore;
+using Elastic.Apm.SqlClient;
+using Elastic.Apm.StackExchange.Redis;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+
+namespace Microsoft.AspNetCore;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void UseBeeApm(this IApplicationBuilder app, IConfiguration configuration = null)
+    {
+        Elastic.Apm.AspNetCore.ApmMiddlewareExtension.UseElasticApm(app, configuration,
+            new HttpDiagnosticsSubscriber(),
+            new EfCoreDiagnosticsSubscriber(),
+            new SqlClientDiagnosticSubscriber(),
+            new ElasticsearchDiagnosticsSubscriber()
+        );
+        UseBeeRedisApm(configuration, app);
+    }
+
+    /// <summary>
+    /// 开启Redis APM 监控
+    /// </summary>
+    private static void UseBeeRedisApm(IConfiguration configuration, IApplicationBuilder app)
+    {
+        var muxer = app.ApplicationServices.GetService<IConnectionMultiplexer>();
+        muxer.UseElasticApm();
+    }
+}
